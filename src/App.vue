29e7ce109ba863436abdcbca504e525caad13a9c
hatemBay <bayoudhhatem5@gmail.com>
2023-08-15 17:22:07 +0100

    added dynamic user change through bearer token,added style changes

--- src/App.vue
+++ src/App.vue
@@ -2,38 +2,20 @@
 import { io } from "socket.io-client";
 import { onBeforeMount, ref } from "vue";
 
-const socketOptions3 = {
+const bearer = ref(
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwic3ViIjoxLCJyb2xlcyI6W3siaWQiOjEsIm5hbWUiOiJBRE1JTiJ9XSwiaWQiOjEsIm5hbWUiOm51bGwsIm9yZ0lkIjoxLCJwZXRzIjpbXSwiaWF0IjoxNjkyMTA2NDIxLCJleHAiOjE2OTIxNjY0MjF9.y5MzxSeGYSQ9kjBxHe_ZUmipD5vnwv9RSS-CsSDYag4"
+);
+let socketOptions = {
   transportOptions: {
     polling: {
       extraHeaders: {
-        Authorization:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwic3ViIjoxLCJyb2xlcyI6W3siaWQiOjEsIm5hbWUiOiJBRE1JTiJ9XSwiaWQiOjEsIm5hbWUiOm51bGwsIm9yZ0lkIjoxLCJwZXRzIjpbXSwiaWF0IjoxNjkyMDE3OTQzLCJleHAiOjE2OTIwNzc5NDN9.BkVt_s4i-xw87UMQr2JrDrSqmWvgllC5BS55YWmgLCM", // 'Bearer h93t4293t49jt34j9rferek...'
-      },
-    },
-  },
-};
-const socketOptions2 = {
-  transportOptions: {
-    polling: {
-      extraHeaders: {
-        Authorization:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluMyIsInN1YiI6MTYsInJvbGVzIjpbeyJpZCI6MSwibmFtZSI6IkFETUlOIn1dLCJpZCI6MTYsIm5hbWUiOm51bGwsIm9yZ0lkIjoxLCJwZXRzIjpbXSwiaWF0IjoxNjkyMDI4MTM0LCJleHAiOjE2OTIwODgxMzR9.xzhiYcYD6UcIFSJ16Kjc9UhYDsOitZhO4eAz2oQ2tsI",
-      },
-    },
-  },
-};
-const socketOptions = {
-  transportOptions: {
-    polling: {
-      extraHeaders: {
-        Authorization:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwic3ViIjoxLCJyb2xlcyI6W3siaWQiOjEsIm5hbWUiOiJBRE1JTiJ9XSwiaWQiOjEsIm5hbWUiOm51bGwsIm9yZ0lkIjoxLCJwZXRzIjpbXSwiaWF0IjoxNjkyMTA2NDIxLCJleHAiOjE2OTIxNjY0MjF9.y5MzxSeGYSQ9kjBxHe_ZUmipD5vnwv9RSS-CsSDYag4",
+        Authorization: bearer.value,
       },
     },
   },
 };
 
-const socket = io("http://localhost:3001", socketOptions);
+let socket = io("http://localhost:3001", socketOptions);
 // const socket2 = io("http://localhost:3003", socketOptions2);
 
 const rooms = ref([]);
@@ -45,6 +27,19 @@ const messageText = ref("");
 const joined = ref(false);
 const username = ref("");
 const typingDisplay = ref("");
+
+const changeUser = () => {
+  socketOptions = {
+    transportOptions: {
+      polling: {
+        extraHeaders: {
+          Authorization: bearer.value,
+        },
+      },
+    },
+  };
+  socket = io("http://localhost:3001", socketOptions);
+};
 
 const findAllRooms = () => {
   socket.emit("findAllRooms", {}, (response) => {
@@ -88,6 +83,7 @@ const sendMessage = (room) => {
 
 let timeout;
 const emitTyping = () => {
+  console.log(socket);
   socket.emit("typing", { isTyping: true });
   timeout = setTimeout(() => {
     socket.emit("typing", { isTyping: false });
@@ -116,47 +112,62 @@ onBeforeMount(() => {
 });
 </script>
 
-<template>
-  <div class="rooms">
-    <div v-for="room in rooms" :key="room.id">
-      <div class="card" style="width: 18rem">
-        <div class="card-body">
-          <h5 class="card-title">{{ room.name }}</h5>
-          <h5 class="card-text">Current users:</h5>
-          <div v-for="user in room.users" :key="user.id">
-            <h5 class="card-text">{{ user.username }}</h5>
-          </div>
-
-          <p class="card-text">
-            Join the room and get access to messaging with current users
-          </p>
-          <!-- <div v-if=""> -->
-          <a @click="join(room, room.id)" class="btn btn-primary">Join</a>
-          <!-- </div> -->
-        </div>
-      </div>
-    </div>
-    <!-- Create a new room -->
-    <form @submit.prevent="newRoom">
-      <label> New room: </label>
-      <input v-model="roomName" />
-      <button type="submit">Create</button>
-    </form>
-    <!-- // Create a new room -->
-  </div>
-
-  <div class="chat">
-    <div v-if="!joined">
-      <form @submit.prevent="join">
-        <label> what's thy name? </label>
-        <input v-model="name" />
+<template class="flex-pos">
+  <div class="row">
+    <div class="col-12">
+      <form @submit.prevent="changeUser">
+        <label> change user? </label>
+        <input v-model="bearer" />
         <button type="submit">Send</button>
       </form>
     </div>
+    <div class="col-12">
+      <div class="rooms">
+        <div v-for="room in rooms" :key="room.id">
+          <div class="card" style="width: 18rem">
+            <div class="card-body">
+              <h5 class="card-title">{{ room.name }}</h5>
+              <h5 class="card-text">Current users:</h5>
+              <div v-for="user in room.users" :key="user.id">
+                <h5 class="card-text">{{ user.username }}</h5>
+              </div>
+
+              <p class="card-text">
+                Join the room and get access to messaging with current users
+              </p>
+              <!-- <div v-if=""> -->
+              <a @click="join(room, room.id)" class="btn btn-primary">Join</a>
+              <!-- </div> -->
+            </div>
+          </div>
+        </div>
+        <!-- Create a new room -->
+        <form @submit.prevent="newRoom">
+          <label> New room: </label>
+          <input v-model="roomName" />
+          <button type="submit">Create</button>
+        </form>
+        <!-- // Create a new room -->
+      </div>
+    </div>
+  </div>
+  <div class="chat">
+    <div v-if="!joined">
+      <label> No room joined! </label>
+    </div>
     <div class="chat-container" v-else>
+      <h5 class="title cap">{{ currentRoom.name }}</h5>
       <div class="messages-container">
-        <div v-for="message in messages" :key="message.user?.id">
-          [{{ username }}]: {{ message.text }}
+        <div
+          class="mt-3 mb-1"
+          v-bind:class="message.user.username !== username ? 'al-right' : null"
+          v-for="message in messages"
+          :key="message.user?.id"
+        >
+          <span class="chat-bg"
+            ><span class="cap">[{{ message.user.username }}]:</span>
+            {{ message.text }}</span
+          >
         </div>
       </div>
 
@@ -177,6 +188,22 @@ onBeforeMount(() => {
 </template>
 
 <style scoped>
+.chat-bg {
+  padding: 5px;
+  margin-bottom: 10px;
+  background-color: rgb(48, 123, 236);
+  color: white;
+  border-radius: 10px;
+}
+.al-right {
+  text-align: right;
+}
+.cap {
+  text-transform: capitalize;
+}
+.title {
+  text-align: center;
+}
 .chat {
   padding: 20px;
   height: 100vh;
@@ -185,9 +212,12 @@ onBeforeMount(() => {
   display: flex;
   flex-direction: column;
   height: 100%;
+  max-width: 80%;
 }
 
 .messages-container {
+  max-height: 50%;
+  overflow-y: scroll;
   flex: 1;
 }
 </style>
